10bfc287289aba2b7fc406e440dd60b499c335a5
SokolAK <sokolak87@gmail.com>
2021-04-22 00:59:07 +0200

    ✔️ ️ update tests

--- test/ref_test_data.f90
+++ test/ref_test_data.f90
@@ -5,41 +5,55 @@ use omp_lib
 use module
 integer :: i 
 
-real ( kind = 8 ) :: cpu_start, cpu_end, wtime_start, wtime_end
-wtime_start = omp_get_wtime()
-call cpu_time(cpu_start)
+integer :: q_sys_start, q_sys_end
+real(kind=8) :: q_wtime_start, q_wtime_end, q_cpu_start, q_cpu_end
+character(len=256) :: q_file
+integer :: th, ths
+real(kind=8) :: cpu_rate
+integer :: count_rate,count_max
+call system_clock(count_rate=count_rate)
+call system_clock(count_max=count_max)
+cpu_rate = real(count_rate)
+th = OMP_GET_THREAD_NUM() + 1
+ths = OMP_GET_NUM_THREADS()
+
+write (q_file, '(A, A)')&
+&'/home/adam.sokol/QCHEM/PROFILING/QAWA&
+&/outs/&
+&qawa.out'
+
+q_wtime_start = omp_get_wtime()
+call cpu_time(q_cpu_start)
+call SYSTEM_CLOCK(q_sys_start)
 
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,I2,I2)')&
-'-> test_data.f90 &
-sub1 S',&
-OMP_GET_THREAD_NUM()+1, OMP_GET_NUM_THREADS()
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3)')&
+&'-> test_data.f90 &
+&sub1 S',&
+&th, ths
+close(10)
 !$OMP END CRITICAL
-
 call qawa_sub1(x, y, z)
 
-call cpu_time(cpu_end)
-wtime_end = omp_get_wtime()
+q_wtime_end = omp_get_wtime()
+call cpu_time(q_cpu_end)
+call SYSTEM_CLOCK(q_sys_end)
 
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,2F14.6)')&
-'<- test_data.f90 &
-sub1 S',&
-cpu_end-cpu_start, wtime_end-wtime_start
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3, 3F14.6)')&
+&'<- test_data.f90 &
+&sub1 S',&
+&th, ths,&
+&(q_sys_end-q_sys_start)/cpu_rate, q_cpu_end-q_cpu_start, &
+&q_wtime_end-q_wtime_start
+close(10)
 !$OMP END CRITICAL
-
 return
 end
 !end qawa wrapper ############################################
@@ -59,42 +73,56 @@ use omp_lib
 use module
 real :: r
 
-real ( kind = 8 ) :: cpu_start, cpu_end, wtime_start, wtime_end
-wtime_start = omp_get_wtime()
-call cpu_time(cpu_start)
+integer :: q_sys_start, q_sys_end
+real(kind=8) :: q_wtime_start, q_wtime_end, q_cpu_start, q_cpu_end
+character(len=256) :: q_file
+integer :: th, ths
+real(kind=8) :: cpu_rate
+integer :: count_rate,count_max
+call system_clock(count_rate=count_rate)
+call system_clock(count_max=count_max)
+cpu_rate = real(count_rate)
+th = OMP_GET_THREAD_NUM() + 1
+ths = OMP_GET_NUM_THREADS()
+
+write (q_file, '(A, A)')&
+&'/home/adam.sokol/QCHEM/PROFILING/QAWA&
+&/outs/&
+&qawa.out'
+
+q_wtime_start = omp_get_wtime()
+call cpu_time(q_cpu_start)
+call SYSTEM_CLOCK(q_sys_start)
 
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,I2,I2)')&
-'-> test_data.f90 &
-sub2 S',&
-OMP_GET_THREAD_NUM()+1, OMP_GET_NUM_THREADS()
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3)')&
+&'-> test_data.f90 &
+&sub2 S',&
+&th, ths
+close(10)
 !$OMP END CRITICAL
-
 call qawa_sub2 &
 (x, y, z)
 
-call cpu_time(cpu_end)
-wtime_end = omp_get_wtime()
+q_wtime_end = omp_get_wtime()
+call cpu_time(q_cpu_end)
+call SYSTEM_CLOCK(q_sys_end)
 
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,2F14.6)')&
-'<- test_data.f90 &
-sub2 S',&
-cpu_end-cpu_start, wtime_end-wtime_start
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3, 3F14.6)')&
+&'<- test_data.f90 &
+&sub2 S',&
+&th, ths,&
+&(q_sys_end-q_sys_start)/cpu_rate, q_cpu_end-q_cpu_start, &
+&q_wtime_end-q_wtime_start
+close(10)
 !$OMP END CRITICAL
-
 return
 end
 !end qawa wrapper ############################################
@@ -113,42 +141,56 @@ subroutine sub3&
 use omp_lib
 real :: r
 
-real ( kind = 8 ) :: cpu_start, cpu_end, wtime_start, wtime_end
-wtime_start = omp_get_wtime()
-call cpu_time(cpu_start)
+integer :: q_sys_start, q_sys_end
+real(kind=8) :: q_wtime_start, q_wtime_end, q_cpu_start, q_cpu_end
+character(len=256) :: q_file
+integer :: th, ths
+real(kind=8) :: cpu_rate
+integer :: count_rate,count_max
+call system_clock(count_rate=count_rate)
+call system_clock(count_max=count_max)
+cpu_rate = real(count_rate)
+th = OMP_GET_THREAD_NUM() + 1
+ths = OMP_GET_NUM_THREADS()
+
+write (q_file, '(A, A)')&
+&'/home/adam.sokol/QCHEM/PROFILING/QAWA&
+&/outs/&
+&qawa.out'
+
+q_wtime_start = omp_get_wtime()
+call cpu_time(q_cpu_start)
+call SYSTEM_CLOCK(q_sys_start)
 
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,I2,I2)')&
-'-> test_data.f90 &
-sub3 S',&
-OMP_GET_THREAD_NUM()+1, OMP_GET_NUM_THREADS()
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3)')&
+&'-> test_data.f90 &
+&sub3 S',&
+&th, ths
+close(10)
 !$OMP END CRITICAL
-
 call qawa_sub3&
 (x, y, z)
 
-call cpu_time(cpu_end)
-wtime_end = omp_get_wtime()
+q_wtime_end = omp_get_wtime()
+call cpu_time(q_cpu_end)
+call SYSTEM_CLOCK(q_sys_end)
 
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,2F14.6)')&
-'<- test_data.f90 &
-sub3 S',&
-cpu_end-cpu_start, wtime_end-wtime_start
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3, 3F14.6)')&
+&'<- test_data.f90 &
+&sub3 S',&
+&th, ths,&
+&(q_sys_end-q_sys_start)/cpu_rate, q_cpu_end-q_cpu_start, &
+&q_wtime_end-q_wtime_start
+close(10)
 !$OMP END CRITICAL
-
 return
 end
 !end qawa wrapper ############################################
@@ -168,41 +210,57 @@ function fun1(x) result(y)
       use omp_lib
 integer :: j
 real :: y
-
 !start qawa open_qawa_fun1 ##################################
-real ( kind = 8 ) :: cpu_start, cpu_end, wtime_start, wtime_end
-wtime_start = omp_get_wtime()
-call cpu_time(cpu_start)
+integer :: q_sys_start, q_sys_end
+real(kind=8) :: q_wtime_start, q_wtime_end, q_cpu_start, q_cpu_end
+character(len=256) :: q_file
+integer :: th, ths
+real(kind=8) :: cpu_rate
+integer :: count_rate,count_max
+call system_clock(count_rate=count_rate)
+call system_clock(count_max=count_max)
+cpu_rate = real(count_rate)
+th = OMP_GET_THREAD_NUM() + 1
+ths = OMP_GET_NUM_THREADS()
+
+write (q_file, '(A, A)')&
+&'/home/adam.sokol/QCHEM/PROFILING/QAWA&
+&/outs/&
+&qawa.out'
+
+q_wtime_start = omp_get_wtime()
+call cpu_time(q_cpu_start)
+call SYSTEM_CLOCK(q_sys_start)
+
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,I2,I2)')&
-'-> test_data.f90 &
-fun1 F',&
-OMP_GET_THREAD_NUM()+1, OMP_GET_NUM_THREADS()
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3)')&
+&'-> test_data.f90 &
+&fun1 F',&
+&th, ths
+close(10)
 !$OMP END CRITICAL
 !end qawa  ##################################
 
 <some code>
-
 !start qawa close_qawa_fun1 ##################################
-call cpu_time(cpu_end)
-wtime_end = omp_get_wtime()
+q_wtime_end = omp_get_wtime()
+call cpu_time(q_cpu_end)
+call SYSTEM_CLOCK(q_sys_end)
+
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,2F14.6)')&
-'<- test_data.f90 &
-fun1 F',&
-cpu_end-cpu_start, wtime_end-wtime_start
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3, 3F14.6)')&
+&'<- test_data.f90 &
+&fun1 F',&
+&th, ths,&
+&(q_sys_end-q_sys_start)/cpu_rate, q_cpu_end-q_cpu_start, &
+&q_wtime_end-q_wtime_start
+close(10)
 !$OMP END CRITICAL
 !end qawa  ##################################
 
@@ -212,60 +270,77 @@ end
 REAL*8 Function fun2(x)
       use omp_lib
 integer :: j
-
 !start qawa open_qawa_fun2 ##################################
-real ( kind = 8 ) :: cpu_start, cpu_end, wtime_start, wtime_end
-wtime_start = omp_get_wtime()
-call cpu_time(cpu_start)
+integer :: q_sys_start, q_sys_end
+real(kind=8) :: q_wtime_start, q_wtime_end, q_cpu_start, q_cpu_end
+character(len=256) :: q_file
+integer :: th, ths
+real(kind=8) :: cpu_rate
+integer :: count_rate,count_max
+call system_clock(count_rate=count_rate)
+call system_clock(count_max=count_max)
+cpu_rate = real(count_rate)
+th = OMP_GET_THREAD_NUM() + 1
+ths = OMP_GET_NUM_THREADS()
+
+write (q_file, '(A, A)')&
+&'/home/adam.sokol/QCHEM/PROFILING/QAWA&
+&/outs/&
+&qawa.out'
+
+q_wtime_start = omp_get_wtime()
+call cpu_time(q_cpu_start)
+call SYSTEM_CLOCK(q_sys_start)
+
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,I2,I2)')&
-'-> test_data.f90 &
-fun2 F',&
-OMP_GET_THREAD_NUM()+1, OMP_GET_NUM_THREADS()
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3)')&
+&'-> test_data.f90 &
+&fun2 F',&
+&th, ths
+close(10)
 !$OMP END CRITICAL
 !end qawa  ##################################
 
 <some code>
-
 !start qawa close_qawa_fun2 ##################################
-call cpu_time(cpu_end)
-wtime_end = omp_get_wtime()
+q_wtime_end = omp_get_wtime()
+call cpu_time(q_cpu_end)
+call SYSTEM_CLOCK(q_sys_end)
+
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,2F14.6)')&
-'<- test_data.f90 &
-fun2 F',&
-cpu_end-cpu_start, wtime_end-wtime_start
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3, 3F14.6)')&
+&'<- test_data.f90 &
+&fun2 F',&
+&th, ths,&
+&(q_sys_end-q_sys_start)/cpu_rate, q_cpu_end-q_cpu_start, &
+&q_wtime_end-q_wtime_start
+close(10)
 !$OMP END CRITICAL
 !end qawa  ##################################
 
 Return
-
 !start qawa close_qawa_fun2 ##################################
-call cpu_time(cpu_end)
-wtime_end = omp_get_wtime()
+q_wtime_end = omp_get_wtime()
+call cpu_time(q_cpu_end)
+call SYSTEM_CLOCK(q_sys_end)
+
 !$OMP CRITICAL
-open(61,file=&
-'/home/adam.sokol/QCHEM/PROFILING/QAWA&
-/outs/&
-qawa.out',&
-action='write',position='append')
-write(61,'(A,2F14.6)')&
-'<- test_data.f90 &
-fun2 F',&
-cpu_end-cpu_start, wtime_end-wtime_start
-close(61)
+open(10,file=&
+&q_file,&
+&action='write',position='append')
+write(10,'(A, 2I3, 3F14.6)')&
+&'<- test_data.f90 &
+&fun2 F',&
+&th, ths,&
+&(q_sys_end-q_sys_start)/cpu_rate, q_cpu_end-q_cpu_start, &
+&q_wtime_end-q_wtime_start
+close(10)
 !$OMP END CRITICAL
 !end qawa  ##################################
 
